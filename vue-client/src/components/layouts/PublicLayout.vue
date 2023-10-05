e9fd9833ef0e76b51e39c7769182f952f671582e
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-10-05 11:40:48 -0500

    feat: add public layout with random background images

--- vue-client/src/components/layouts/PublicLayout.vue
+++ vue-client/src/components/layouts/PublicLayout.vue
@@ -1,10 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import dew from '../../assets/images/backgrounds/dew.jpg';
+import kromlau from '../../assets/images/backgrounds/kromlau.jpg';
+import marsh from '../../assets/images/backgrounds/marsh.jpg';
+
+const images = [dew, kromlau, marsh];
+const randomImage = images[Math.floor(Math.random() * images.length)];
+</script>
 
 <template>
-  <div>
-    <h1>Public Layout</h1>
+  <main class="container" :style="{ backgroundImage: `url(${randomImage})` }">
     <router-view></router-view>
-  </div>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+</style>
